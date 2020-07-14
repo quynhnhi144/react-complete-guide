f05c254c924d3f41d0e1e05defd1fb5575f7ce79
quynhnhi144 <ltqnhi97@gmail.com>
2020-07-14 23:19:18 +0700

    working with inline styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import Person from './Person/Person';
-import './App.css';
+import React, { Component } from "react";
+import Person from "./Person/Person";
+import "./App.css";
 
 class App extends Component {
-
   state = {
     persons: [
       { name: "Max", age: 28 },
       { name: "Manu", age: 26 },
       { name: "Stephanie", age: 24 },
-    ]
-  }
+    ],
+  };
 
   switchNameHandler = (newName) => {
     // console.log('Was clicked!');
@@ -20,9 +19,9 @@ class App extends Component {
         { name: newName, age: 28 },
         { name: "Manu", age: 26 },
         { name: "Stephanie", age: 25 },
-      ]
-    })
-  }
+      ],
+    });
+  };
 
   nameChangedHandler = (event) => {
     this.setState({
@@ -30,34 +29,55 @@ class App extends Component {
         { name: "Max", age: 28 },
         { name: event.target.value, age: 26 },
         { name: "Stephanie", age: 24 },
-      ]
-    })
-  }
+      ],
+    });
+  };
 
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    };
+
     return (
       <div className="App">
         <h1>Hi I'm React</h1>
         <p>This is working!!!</p>
-        <button onClick={() => { this.switchNameHandler("Maximillan") }}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <button
+          style={style}
+          onClick={() => {
+            this.switchNameHandler("Maximillan");
+          }}
+        >
+          Switch name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
           click={this.switchNameHandler.bind(this, "Max!")}
           changed={this.nameChangedHandler}
-        >My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
 
-      // It hasn'tlocation in component scope ==> Error 
+      // It hasn'tlocation in component scope ==> Error
       //<p>This is working!!!</p>
     );
     // This is some things will be compiled
     //return React.createElement('div', { className: 'App' }, null, React.createElement('h1', null, 'Hi I\'m React'));
-
   }
-
 }
 
 export default App;
