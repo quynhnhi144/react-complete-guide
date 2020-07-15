33298b9e6a7a6ca731f49e120a9f6eb838e47209
quynhnhi144 <ltqnhi97@gmail.com>
2020-07-15 10:37:08 +0700

    rendering content conditionally

--- src/App.js
+++ src/App.js
@@ -1,67 +1,88 @@
 import React, { Component } from "react";
-import UserInput from "./UserInput/UserInput";
-import UserOutput from "./UserOutPut/UserOutput";
+import Person from "./Person/Person";
 import "./App.css";
 
 class App extends Component {
   state = {
-    username: "superMax",
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 26 },
+      { name: "Stephanie", age: 24 },
+    ],
+    otherState: "some other value",
+    showPersons: false,
   };
 
-  usernameChangedHandler = (event) => {
-    this.setState({ username: event.target.value });
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = "Maximillan";
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Manu", age: 26 },
+        { name: "Stephanie", age: 25 },
+      ],
+    });
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 26 },
+        { name: "Stephanie", age: 24 },
+      ],
+    });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   };
 
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    };
+
     return (
       <div className="App">
-        <ol>
-          <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>
-            UserInput should hold an input element, UserOutput two paragraphs
-          </li>
-          <li>
-            Output multiple UserOutput components in the App component (any
-            paragraph texts of your choice)
-          </li>
-          <li>
-            Pass a username (of your choice) to UserOutput via props and display
-            it there
-          </li>
-          <li>
-            Add state to the App component (= the username) and pass the
-            username to the UserOutput component
-          </li>
-          <li>
-            Add a method to manipulate the state (= an event-handler method)
-          </li>
-          <li>
-            Pass the event-handler method reference to the UserInput component
-            and bind it to the input-change event
-          </li>
-          <li>
-            Ensure that the new input entered by the user overwrites the old
-            username passed to UserOutput
-          </li>
-          <li>
-            Add two-way-binding to your input (in UserInput) to also display the
-            starting username
-          </li>
-          <li>
-            Add styling of your choice to your components/ elements in the
-            components - both with inline styles and stylesheets
-          </li>
-        </ol>
-
-        <UserInput
-          changed={this.usernameChangedHandler}
-          currentName={this.state.username}
-        ></UserInput>
-        <UserOutput userName={this.state.username}></UserOutput>
-        <UserOutput userName={this.state.username}></UserOutput>
-        <UserOutput userName="Max"></UserOutput>
+        <h1>Hi I'm React</h1>
+        <p>This is working!!!</p>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {this.state.showPersons === true ? (
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, "Max!")}
+              changed={this.nameChangedHandler}
+            >
+              My Hobbies: Racing
+            </Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+            />
+          </div>
+        ) : null}
       </div>
+
+      // It hasn'tlocation in component scope ==> Error
+      //<p>This is working!!!</p>
     );
+    // This is some things will be compiled
+    //return React.createElement('div', { className: 'App' }, null, React.createElement('h1', null, 'Hi I\'m React'));
   }
 }
 
