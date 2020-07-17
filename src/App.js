58452296152125ba93ae55f8d2827b111b504b6f
quynhnhi144 <ltqnhi97@gmail.com>
2020-07-17 13:43:40 +0700

    enabling and using css modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import Radium, { StyleRoot } from "radium";
 
 import Person from "./Person/Person";
-import "./App.css";
+import classes from "./App.css";
 
 class App extends Component {
   state = {
@@ -83,19 +82,19 @@ class App extends Component {
       style.backgroundColor = "red";
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push("red"); // classes = ['red']
+      assignedClasses.push(classes.red); // classes = ['red']
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push("bold"); // classes = ['red' ,'bold']
+      assignedClasses.push(classes.bold); // classes = ['red' ,'bold']
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi I'm React</h1>
-        <p className={classes.join(" ")}>This is working!!!</p>
+        <p className={assignedClasses.join(" ")}> This is working!!!</p>
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
