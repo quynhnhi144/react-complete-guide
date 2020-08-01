dc7fa80eb6dd822a79ac57558574ca5f2703b13d
Nhi Le <nhi_le@datahouse.asia>
2020-08-01 18:41:24 +0700

    splitting an app into components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-
-import Person from "../components/Persons/Person/Person";
 import classes from "./App.css";
+
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
   state = {
@@ -53,52 +54,26 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let btnClass = "";
 
     if (this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-              ></Person>
-            );
-          })}
-        </div>
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
       );
-
-      btnClass = classes.Red;
     }
-
-    let assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
-
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red' ,'bold']
-    }
-
     return (
       <div className={classes.App}>
-        <h1>Hi I'm React</h1>
-        <p className={assignedClasses.join(" ")}> This is working!!!</p>
-        <button className={btnClass} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
-
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
-      // It hasn'tlocation in component scope ==> Error
-      //<p>This is working!!!</p>
     );
-    // This is some things will be compiled
-    //return React.createElement('div', { className: 'App' }, null, React.createElement('h1', null, 'Hi I\'m React'));
   }
 }
 
