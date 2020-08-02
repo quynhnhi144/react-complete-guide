1fe34f342ee0a842d218aee9d3a72bf514f6a0dc
Nhi Le <nhi_le@datahouse.asia>
2020-08-02 22:28:08 +0700

    component creation lifecycle in action

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Person from "./Person/Person";
 
-const persons = (props) =>
-  props.persons.map((person, index) => {
+const persons = (props) => {
+  console.log("[Persons.js] redering...");
+  return props.persons.map((person, index) => {
     return (
       <Person
         click={() => props.clicked(index)}
@@ -10,8 +11,9 @@ const persons = (props) =>
         age={person.age}
         key={person.id}
         changed={(event) => props.changed(event, person.id)}
-      ></Person>
+      />
     );
   });
+};
 
 export default persons;
